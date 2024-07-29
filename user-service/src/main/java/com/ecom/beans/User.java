bdca3d7b180192a975ffba4d2d5a58f1c3e2d3e9
Vikash <yequalstovikash@gmail.com>
2024-07-29 22:46:14 +0530

    user service setup

---
+++ user-service/src/main/java/com/ecom/beans/User.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.ecom.beans;
+
+/**
+ * 
+ */
+public class User {
+
+	private Integer userId;
+	private String firstName;	
+	private String lastName;
+	private char[] password;
+	private String email;
+	private String phoneNum;
+	
+	public User(Integer userId, String firstName, String lastName, char[] password, String email, String phoneNum) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.email = email;
+		this.phoneNum = phoneNum;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public char[] getPassword() {
+		return password;
+	}
+
+	public void setPassword(char[] password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+	
+	
+	
+}
