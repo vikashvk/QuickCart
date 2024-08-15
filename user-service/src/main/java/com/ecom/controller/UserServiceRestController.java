1eccb67ee9d92734ccbbc4c96a8c1441a8580109
Vikash <yequalstovikash@gmail.com>
2024-08-15 21:29:53 +0530

    user impl

--- user-service/src/main/java/com/ecom/controller/UserServiceRestController.java
+++ user-service/src/main/java/com/ecom/controller/UserServiceRestController.java
@@ -3,13 +3,20 @@
  */
 package com.ecom.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecom.service.UserValidationImpl;
 
 /**
  * 
  */
 
 @RestController
+@RequestMapping("/user")
 public class UserServiceRestController {
+	
+	private UserValidationImpl userService;
+	
 
 }
