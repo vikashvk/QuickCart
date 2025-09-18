20510e90ee48680233b7f6ef4ac046c6d7eb55c6
vaishvi02 <vaishvimathur@gmail.com>
2025-09-18 10:36:25 +0530

    Product serive with mysql and sts setup

--- product-service/src/main/java/com/ecom/beans/Product.java
+++ product-service/src/main/java/com/ecom/beans/Product.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 
 /**
  * 
+ * 
  */
 @Entity
 public class Product {
--- product-service/src/main/java/com/ecom/product_service/ProductServiceApplication.java
+++ product-service/src/main/java/com/ecom/product_service/ProductServiceApplication.java
@@ -3,11 +3,11 @@ package com.ecom.product_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
 }
