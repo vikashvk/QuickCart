cfdb3c8b8f476d8b861933dad04adbc280a0a80a
vikashvk <yequalstovikash@gmail.com>
2026-03-17 23:11:36 +0530

    record

---
+++ order-service/src/main/java/com/ecom/order_service/dto/OrderRequest.java
@@ -0,0 +1,7 @@
+package com.ecom.order_service.dto;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer qty) {
+
+}
--- order-service/src/main/java/com/ecom/order_service/service/OrderService.java
+++ order-service/src/main/java/com/ecom/order_service/service/OrderService.java
@@ -1,7 +1,21 @@
 package com.ecom.order_service.service;
 
-public class OrderService {
-    public void placeOrder(Order){
+import com.ecom.order_service.dto.OrderRequest;
+import com.ecom.order_service.model.Order;
+import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
+@Service
+public class OrderService {
+    public void placeOrder(OrderRequest orderRequest){
+//map orderRequest to Order Object
+Order order =new Order();
+order.setOrderNumber(UUID.randomUUID().toString());
+order.setPrice(orderRequest.price());
+order.setQuantity(orderRequest.qty());
+order.setSkuCode(orderRequest.skuCode());
+        //save order to repo
+order.
     }
 }
