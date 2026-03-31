51c14871981d82b7129a0543c06238e19e2427aa
vikashvk <yequalstovikash@gmail.com>
2026-03-31 14:28:20 +0530

    Controller for saving data

---
+++ order-service/src/main/java/com/ecom/order/controller/OrderController.java
@@ -0,0 +1,21 @@
+package com.ecom.order.controller;
+
+import com.ecom.order.dto.OrderRequest;
+import com.ecom.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/order")
+public class OrderController {
+    public final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+
+}
