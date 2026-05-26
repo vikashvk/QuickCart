de2c3ec523ce0a3c198751eae5ebbc0d05a5012b
vikashvk <yequalstovikash@gmail.com>
2026-05-26 22:17:00 +0530

    Repository added

---
+++ inventory-service/src/main/java/com/ecom/microservices/inventory_service/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.ecom.microservices.inventory_service.repository;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventoryRepository {
+    
+}
