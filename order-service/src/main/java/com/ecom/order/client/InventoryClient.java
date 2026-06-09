7aa805bfc71843466320ac1785ab7f98015275aa
vikashvk <yequalstovikash@gmail.com>
2026-06-09 21:32:04 +0530

    Added inventory service in order service

--- order-service/src/main/java/com/ecom/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ecom/order/client/InventoryClient.java
@@ -1,7 +1,12 @@
 package com.ecom.order.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value="inventory",url="")
+@FeignClient(value="inventory",url="https://localhost:8082")
 public interface InventoryClient {
+    @RequestMapping(method = RequestMethod.GET,value="api/inventory")
+    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 }
--- order-service/src/main/java/com/ecom/order/service/OrderService.java
+++ order-service/src/main/java/com/ecom/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ecom.order.service;
 
+import com.ecom.order.client.InventoryClient;
 import com.ecom.order.dao.OrderRepository;
 import com.ecom.order.dto.OrderRequest;
 import com.ecom.order.model.Order;
@@ -14,16 +15,23 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository orderRepository;
+private final InventoryClient inventoryClient;
     public void placeOrder(OrderRequest orderRequest) {
+        boolean isInStock= inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.qty());
         //map orderRequest to Order Object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.qty());
-        order.setSkuCode(orderRequest.skuCode());
+        if(isInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.qty());
+            order.setSkuCode(orderRequest.skuCode());
 
-        //save order to repo
-        orderRepository.save(order);
-        log.info("Saved in repo");
+            //save order to repo
+            orderRepository.save(order);
+            log.info("Saved in repo");
+        }
+        else {
+            throw new RuntimeException("Product with skuCode "+orderRequest.skuCode()+" is Out of Stock");
+        }
     }
 }
