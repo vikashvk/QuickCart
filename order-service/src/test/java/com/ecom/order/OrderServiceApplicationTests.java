fde23e31a937c6f8feb24ab3abd24a36fea035fc
vikashvk <yequalstovikash@gmail.com>
2026-06-12 23:44:41 +0530

    Added wiremock for testing

--- order-service/src/test/java/com/ecom/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/ecom/order/OrderServiceApplicationTests.java
@@ -2,11 +2,21 @@ package com.ecom.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
+
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
 
 	@Test
 	void contextLoads() {
