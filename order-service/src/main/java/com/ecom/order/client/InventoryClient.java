18dea4ba95104ef80c98e67be410409ed2a88ecf
vikashvk <yequalstovikash@gmail.com>
2026-06-13 17:19:48 +0530

    quantity missing in request

--- order-service/src/main/java/com/ecom/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ecom/order/client/InventoryClient.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value="inventory",url="https://localhost:8082")
+@FeignClient(value="inventory",url="http://localhost:8082")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET,value="api/inventory")
-    boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+    @RequestMapping(method = RequestMethod.GET,value="/api/inventory")
+    boolean isInStock(@RequestParam("skuCode") String skuCode, @RequestParam("quantity") Integer quantity);
 }
