06e5214b8ec5bf430355663358feeb51901458ab
vikashvk <yequalstovikash@gmail.com>
2026-06-14 22:27:42 +0530

    Add integration test for Submit Order

--- order-service/src/test/java/com/ecom/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/ecom/order/OrderServiceApplicationTests.java
@@ -1,5 +1,8 @@
 package com.ecom.order;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -7,6 +10,8 @@ import org.springframework.boot.testcontainers.service.connection.ServiceConnect
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,8 +23,39 @@ class OrderServiceApplicationTests {
 	@LocalServerPort
 	private Integer port;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 	}
+
+	static {
+		mySQLContainer.start();
+	}
+	@Test
+	void shouldSubmitOrder() {
+		String submitOrderJson = """
+                {
+                     "skuCode": "iphone_15",
+                     "price": 1000,
+                     "quantity": 1
+                }
+                """;
+
+		InventoryStubs.stubInventoryCall("iphone_15", 1);
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrderJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+
+		assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+	}
+
 
 }
