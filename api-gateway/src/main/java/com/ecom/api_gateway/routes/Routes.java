98f00a0a222e9d7efacb05291f576245e5de7957
vikashvk <yequalstovikash@gmail.com>
2026-07-18 19:42:57 +0530

    Gateway applied for all 3 services on 9000 port

---
+++ api-gateway/src/main/java/com/ecom/api_gateway/routes/Routes.java
@@ -0,0 +1,33 @@
+package com.ecom.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
+        return GatewayRouterFunctions.route("product_service").route(RequestPredicates.path("/api/product/**"),
+                        http("http://localhost:8080")).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service").route(RequestPredicates.path("/api/order/**"),
+                http("http://localhost:8081")).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
+        return GatewayRouterFunctions.route("inventory_service").route(RequestPredicates.path("/api/inventory/**"),
+                http("http://localhost:8082")).build();
+    }
+}
