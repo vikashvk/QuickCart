a80404b103a761d5d17ee3f674154d3d7f441574
vikashvk <yequalstovikash@gmail.com>
2026-07-22 21:36:39 +0530

    security files

---
+++ api-gateway/src/main/java/com/ecom/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.ecom.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(SecurityConfig){
+
+    }
+}
