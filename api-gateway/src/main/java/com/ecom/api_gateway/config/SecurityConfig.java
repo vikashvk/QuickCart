8f924761e875c5ee6604d6980a3f20a219814f83
vikashvk <yequalstovikash@gmail.com>
2026-07-24 20:46:02 +0530

    Security added at gateway

--- api-gateway/src/main/java/com/ecom/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ecom/api_gateway/config/SecurityConfig.java
@@ -2,11 +2,20 @@ package com.ecom.api_gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(SecurityConfig){
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
     }
 }
