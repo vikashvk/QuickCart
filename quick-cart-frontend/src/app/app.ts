acafad049a3219dd0345b171a38202a105106853
vikashvk <yequalstovikash@gmail.com>
2026-08-04 22:17:49 +0530

    Added login/logout

--- quick-cart-frontend/src/app/app.ts
+++ quick-cart-frontend/src/app/app.ts
@@ -1,13 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal,inject,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { OidcSecurityService } from 'angular-auth-oidc-client';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('quick-cart-frontend');
-  
+    private readonly oidcSecurityService = inject(OidcSecurityService);
+
+    ngOnInit(): void {
+    this.oidcSecurityService
+      .checkAuth()
+      .subscribe(({isAuthenticated}) => {
+        console.log('app authenticated', isAuthenticated);
+      })
+  }
 }
