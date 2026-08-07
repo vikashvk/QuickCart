4c5257a8dd98e552f28576f85b530fc2e93a5120
vikashvk <yequalstovikash@gmail.com>
2026-08-07 13:30:46 +0530

    bearer token missing in req

--- quick-cart-frontend/src/app/app.config.ts
+++ quick-cart-frontend/src/app/app.config.ts
@@ -1,17 +1,20 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { authConfig } from './config/auth.config';
 import { provideAuth } from 'angular-auth-oidc-client';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptor/auth.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([authInterceptor])),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(),
-        provideAuth(authConfig),
-
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAuth(authConfig),
   ]
 };
