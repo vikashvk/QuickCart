8ce1753bcd905fdb3a7227b3ec07950570d46dbd
vikashvk <yequalstovikash@gmail.com>
2026-08-27 00:31:14 +0530

    permit urls for swagger urls

--- api-gateway/src/main/java/com/ecom/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/ecom/api_gateway/config/SecurityConfig.java
@@ -15,11 +15,14 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**", "/actuator/prometheus"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/api/product/**").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers(freeResourceUrls).permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
