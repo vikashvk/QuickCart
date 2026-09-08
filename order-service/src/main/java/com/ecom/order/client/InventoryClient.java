91829a6d4c589c7c1c19f9e9d9defe42dbe8b764
vikashvk <yequalstovikash@gmail.com>
2026-09-08 11:25:37 +0530

    openAPI new

--- order-service/src/main/java/com/ecom/order/client/InventoryClient.java
+++ order-service/src/main/java/com/ecom/order/client/InventoryClient.java
@@ -1,12 +1,9 @@
 package com.ecom.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value="inventory",url="${inventory.url}")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET,value="/api/inventory")
+    @GetExchange("/api/inventory")
     boolean isInStock(@RequestParam("skuCode") String skuCode, @RequestParam("quantity") Integer quantity);
 }
