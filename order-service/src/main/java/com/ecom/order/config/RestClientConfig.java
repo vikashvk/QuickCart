a536ef8de647e6f134824a300f24c55d00438b81
vikashvk <yequalstovikash@gmail.com>
2026-09-10 22:18:11 +0530

    RestTemplate for inventory

--- order-service/src/main/java/com/ecom/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/ecom/order/config/RestClientConfig.java
@@ -1,13 +1,24 @@
 package com.ecom.order.config;
 
 import com.ecom.order.client.InventoryClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
 public class RestClientConfig {
+    @Value("${inventory.url}")
+    private String inventoryServiceUrl="http://localhost:8082";
     @Bean
     public InventoryClient inventoryClient(){
-        
+        RestClient restClient=RestClient.builder()
+                .baseUrl(inventoryServiceUrl)
+                .build();
+        var restClientAdaptor= RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory= HttpServiceProxyFactory.builderFor(restClientAdaptor).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
 }
